cb6311020a18fb2ed62461bdd49a4e7a4ffd5f8e
YijingJia <u7566045@anu.edu.au>
2023-09-14 19:30:57 +1000

    Add MainMenu

---
+++ src/app/src/main/java/com/example/antiscam/AntiScam.java
@@ -0,0 +1,30 @@
+package com.example.antiscam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AntiScam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_antiscam);
+
+        // Capture ADD URL button from layout
+        Button gotoMainMenuBtn = (Button) findViewById(R.id.testMainMenu);
+        // Register the onClick listener
+        gotoMainMenuBtn.setOnClickListener(gotoMainMenuListener);
+    }
+
+    private View.OnClickListener gotoMainMenuListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent toMainMenuInt = new Intent(getApplicationContext(), MainMenu.class);
+            startActivity(toMainMenuInt);
+        }
+    };
+}
