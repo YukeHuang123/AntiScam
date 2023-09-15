834e12f6cb2bccf09ec2f0fb7114795629a87915
YijingJia <u7566045@anu.edu.au>
2023-09-15 23:09:02 +1000

    Update main menu

--- src/app/src/main/java/com/example/antiscam/MainMenu.java
+++ src/app/src/main/java/com/example/antiscam/MainMenu.java
@@ -48,13 +48,13 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         // Data List for scam case card and set scam case data to adapter
-//        List<scamCase> dataList = createDataList();
         List<scamCase> dataList = scamCaseListAccess.loadJsonData(this, "scamCase.json");
         cardAdapter = new mainMenuCardAdapter(dataList);
 
         // Initialize recyclerView
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
+
         // Set adapter for recyclerView to display scam list cards
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(cardAdapter);
@@ -63,51 +63,5 @@ public class MainMenu extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.avatarImgView);
         UserInfoManager.getUserInfo(this, userNameView, imageView);
 
-//        // Get user information from firebase
-//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//        if (user != null) {
-//            // Name, email address, and profile photo Url
-//
-//            String username = user.getDisplayName();
-////            String email = user.getEmail();
-//            Uri photoUrl = user.getPhotoUrl();
-//
-//            TextView userNameView = findViewById(R.id.userName);
-//            userNameView.setText(username);
-//
-//            // Check if user's email is verified
-////            boolean emailVerified = user.isEmailVerified();
-//
-//            // The user's ID, unique to the Firebase project. Do NOT use this value to
-//            // authenticate with your backend server, if you have one. Use
-//            // FirebaseUser.getIdToken() instead.
-//            String uid = user.getUid();
-//
-//            FirebaseStorage storage = FirebaseStorage.getInstance();
-//            StorageReference storageRef = storage.getReference();
-//
-//            // Set path
-//            String imagePath = String.valueOf(photoUrl); // 替换为你的存储路径
-//
-//            // Get StorageReference
-//            StorageReference imageRef = storageRef.child(imagePath);
-//
-//            // Download image and load to ImageView
-//            ImageView imageView = findViewById(R.id.avatarImgView); // 替换为你的 ImageView
-//
-//            imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                @Override
-//                public void onSuccess(Uri uri) {
-//                    // Use Picasso to load and transform image
-//                    Picasso.get().load(uri.toString()).transform(new CircleImageTransformer()).into(imageView);
-//                }
-//            }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception exception) {
-//                    imageView.setImageResource(R.drawable.default_avatar);
-//                }
-//            });
-//
-//        }
     }
 }
