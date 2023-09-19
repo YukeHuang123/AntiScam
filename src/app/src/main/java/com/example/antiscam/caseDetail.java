611d5629cd67c63d04127bf31869bd7c7453be8a
Coco Huang <u7618794@anu.edu.au>
2023-09-19 18:11:47 +1000

    create caseDetail activity

---
+++ src/app/src/main/java/com/example/antiscam/caseDetail.java
@@ -0,0 +1,14 @@
+package com.example.antiscam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class caseDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case_detail);
+    }
+}
