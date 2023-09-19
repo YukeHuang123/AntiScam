bed41030396ae2d316287612b06a8bcc36246527
Coco Huang <u7618794@anu.edu.au>
2023-09-20 00:31:34 +1000

    update case detail

--- src/app/src/main/java/com/example/antiscam/caseDetail.java
+++ src/app/src/main/java/com/example/antiscam/caseDetail.java
@@ -2,7 +2,10 @@ package com.example.antiscam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class caseDetail extends AppCompatActivity {
 
@@ -11,4 +14,9 @@ public class caseDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_detail);
     }
+    public void leave(View v){
+        Intent intent=new Intent(caseDetail.this, MainMenu.class);
+        startActivity(intent);
+    }
+
 }
