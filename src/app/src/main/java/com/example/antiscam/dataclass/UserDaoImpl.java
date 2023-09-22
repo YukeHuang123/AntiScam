b58d34fac3f6b8469f2f2f51558dd0e5fa4ed3a7
YijingJia <u7566045@anu.edu.au>
2023-09-22 21:03:48 +1000

    Update userDao, add scamcase Dao

--- src/app/src/main/java/com/example/antiscam/dataclass/UserDaoImpl.java
+++ src/app/src/main/java/com/example/antiscam/dataclass/UserDaoImpl.java
@@ -5,8 +5,10 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.example.antiscam.bean.User;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -23,21 +25,20 @@ public class UserDaoImpl implements UserDao {
     public void getUserByEmail(String targetEmail, UserDao.UserCallback userCallback) {
         usersCollection.whereEqualTo("email", targetEmail)
                 .get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                            // get document data
-                            User user = document.toObject(User.class);
-                            // pass user class to callback function
-                            userCallback.onUserReceived(user);
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                // get document data
+                                User user = document.toObject(User.class);
+                                // pass user class to callback function
+                                userCallback.onUserReceived(user);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
                         }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, "Error querying users by email", e);
                     }
                 });
     }
