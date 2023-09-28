5b024e3971b971524cb7ed1f2a741034433810df
Zhaoyun Xu <u7558707@anu.edu.au>
2023-09-28 17:51:09 +1000

    add cache structure

---
+++ src/app/src/main/java/com/example/antiscam/tool/AVLTree.java
@@ -0,0 +1,144 @@
+package com.example.antiscam.tool;
+
+public class AVLTree<K extends Comparable<K>, V> extends Tree<K, V> {
+
+    private int height(Node<K, V> node) {
+        if (node == null) {
+            return -1;
+        } else {
+            return node.height;
+        }
+    }
+
+    private int getBalance(Node<K, V> node) {
+        if (node == null) {
+            return 0;
+        } else {
+            int leftHeight = height(node.left);
+            int rightHeight = height(node.right);
+            return leftHeight - rightHeight;
+        }
+    }
+
+
+    protected Node<K, V> add(Node<K, V> node, K key, V value) {
+        if (node == null) return new Node<>(key, value);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = add(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value; // overwrite existing value
+        }
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        // Left heavy
+        if (balance > 1) {
+            // Check for "Left-Right" case
+            if (key.compareTo(node.left.key) > 0) {
+                node.left = singleRotateWithLeft(node.left);
+                return singleRotateWithRight(node);
+            }
+            // "Left-Left" case
+            else {
+                return singleRotateWithRight(node);
+            }
+        }
+
+        // Right heavy
+        if (balance < -1) {
+            // Check for "Right-Left" case
+            if (key.compareTo(node.right.key) < 0) {
+                node.right = singleRotateWithRight(node.right);
+                return singleRotateWithLeft(node);
+            }
+            // "Right-Right" case
+            else {
+                return singleRotateWithLeft(node);
+            }
+        }
+
+        return node;
+    }
+
+
+    @Override
+    protected Node<K, V> remove(Node<K, V> node, K key) {
+        if (node == null) return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(node.left, key);
+        } else if (cmp > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.left == null || node.right == null) {
+                Node<K, V> temp = (node.left != null) ? node.left : node.right;
+                node = temp;
+            } else {
+                Node<K, V> temp = min(node.right);
+                node.key = temp.key;
+                node.value = temp.value;
+                node.right = remove(node.right, temp.key);
+            }
+        }
+
+        if (node == null) return null;
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        // Left heavy
+        if (balance > 1) {
+            // Check for "Left-Right" case
+            if (getBalance(node.left) < 0) {
+                node.left = singleRotateWithLeft(node.left);
+                return singleRotateWithRight(node);
+            }
+            // "Left-Left" case
+            else {
+                return singleRotateWithRight(node);
+            }
+        }
+
+        // Right heavy
+        if (balance < -1) {
+            // Check for "Right-Left" case
+            if (getBalance(node.right) > 0) {
+                node.right = singleRotateWithRight(node.right);
+                return singleRotateWithLeft(node);
+            }
+            // "Right-Right" case
+            else {
+                return singleRotateWithLeft(node);
+            }
+        }
+
+        return node;
+    }
+
+
+    private Node<K, V> singleRotateWithLeft(Node<K, V> k2) {
+        Node<K, V> k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = 1 + Math.max(height(k2.left), height(k2.right));
+        k1.height = 1 + Math.max(height(k1.left), k2.height);
+        return k1;
+    }
+
+    private Node<K, V> singleRotateWithRight(Node<K, V> k1) {
+        Node<K, V> k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        k1.height = 1 + Math.max(height(k1.left), height(k1.right));
+        k2.height = 1 + Math.max(height(k2.right), k1.height);
+        return k2;
+    }
+}
---
+++ src/app/src/main/java/com/example/antiscam/tool/LRUCache.java
@@ -0,0 +1,94 @@
+package com.example.antiscam.tool;
+
+import java.util.HashMap;
+
+public class LRUCache<K, V> {
+    private final int capacity;
+    private final HashMap<K, Node<K, V>> map;
+    private final DoublyLinkedList<K, V> list;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.list = new DoublyLinkedList<>();
+    }
+
+    public V get(K key) {
+        if (!map.containsKey(key)) return null;
+
+        Node<K, V> node = map.get(key);
+        list.moveToFront(node);
+        return node.value;
+    }
+
+    public void put(K key, V value) {
+        if (map.containsKey(key)) {
+            Node<K, V> node = map.get(key);
+            node.value = value;
+            list.moveToFront(node);
+        } else {
+            if (map.size() == capacity) {
+                Node<K, V> tail = list.removeTail();
+                map.remove(tail.key);
+            }
+            Node<K, V> newNode = new Node<>(key, value);
+            map.put(key, newNode);
+            list.addToFront(newNode);
+        }
+    }
+
+    private static class DoublyLinkedList<K, V> {
+        Node<K, V> head;
+        Node<K, V> tail;
+
+        public void moveToFront(Node<K, V> node) {
+            if (node == head) return;
+            remove(node);
+            addToFront(node);
+        }
+
+        public void addToFront(Node<K, V> node) {
+            node.prev = null;
+            node.next = head;
+            if (head != null) {
+                head.prev = node;
+            }
+            head = node;
+            if (tail == null) {
+                tail = head;
+            }
+        }
+
+        public Node<K, V> removeTail() {
+            Node<K, V> res = tail;
+            remove(tail);
+            return res;
+        }
+
+        public void remove(Node<K, V> node) {
+            if (node.prev != null) {
+                node.prev.next = node.next;
+            } else {
+                head = node.next;
+            }
+
+            if (node.next != null) {
+                node.next.prev = node.prev;
+            } else {
+                tail = node.prev;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> prev;
+        Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
