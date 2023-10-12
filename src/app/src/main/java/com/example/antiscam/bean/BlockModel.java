f7ed7f2afca8bce79c7b42a3852a1632a92f07d4
Yijing Jia <yijingjia@hotmail.com>
2023-10-12 15:58:56 +1100

    update block

---
+++ src/app/src/main/java/com/example/antiscam/bean/BlockModel.java
@@ -0,0 +1,20 @@
+package com.example.antiscam.bean;
+
+public class BlockModel {
+    private String email;
+
+    public BlockModel() {
+    }
+
+    public BlockModel(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/app/src/main/java/com/example/antiscam/dataclass/BlockManager.java
@@ -0,0 +1,43 @@
+package com.example.antiscam.dataclass;
+
+import static com.example.antiscam.act.ChatActivity.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockManager {
+    static List<String> blockedUsers = new ArrayList<>();
+    public static List<String> getBlockedUsers(String blockerEmail){
+        //
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //
+        DocumentReference docRef = db.collection("block").document(blockerEmail);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        //
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        return blockedUsers;
+    }
+}
